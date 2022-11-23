934a6e07130f9a7c979e54fe80015f9ca016adb0
jamiestungo <jamiestungo@gmail.com>
2022-11-23 09:21:13 +0000

    models and routes updated with GET and POST requests, PING functionality deleted

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
 import { query } from '../db/index.js';
 
 export async function getApis() {
-    const response = await query(`SELECT * FROM API_LIST`);
+    const response = await query(`SELECT * FROM API_LIST INNER JOIN API_RESPONSE ON api_list.api_id=api_response.api_id`);
     return response.rows;
 }
 
 export async function createApi(api) {
     const response = await query(`INSERT INTO API_LIST (api_name, api_url, tags, doclink) VALUES ($1, $2, $3, $4) RETURNING *`, [api.api_name, api.api_url, api.tags, api.doclink]);
+    const response2 = await query(`INSERT INTO API_RESPONSE (api_id) VALUES ($1) RETURNING *`,[response.rows[0].api_id]);
     return response.rows[0];
 }
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,16 @@ const router = express.Router();
 
 router.get("/", async function (req, res){
     const result = await getApis();
+    console.log(result);
+    if(result == undefined) { return res.json({success:false})}
     return res.json({success:true, payload: result})
     //return res.json({success:true, payload: '1-0 to the Scorers'})
 })
 
 router.post("/", async function(req, res){
+    if(req.body == undefined) { return res.status(400).json({success:false})};
     const result = await createApi(req.body);
-    return res.json({success:true, payload: result})
+    return res.status(201).json({success:true, payload: result})
 })
 
 export {router};
