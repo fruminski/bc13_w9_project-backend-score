408f58bfeb5bfacdb5b240260a00b08b63aba8fd
jamiestungo <jamiestungo@gmail.com>
2022-11-24 13:56:08 +0000

    Non functioning urls now error handle correctly

--- models/index.js
+++ models/index.js
@@ -6,37 +6,42 @@ export async function getApis() {
     const response = await query(`SELECT * FROM API_LIST INNER JOIN API_RESPONSE ON api_list.api_id=api_response.api_id`);
 
     let api;
-    let json;
 
     for(api of response.rows) {
-
-        let get_success = false;
-        //  
-        let fetchResponse = await fetch(api.api_url)
-        .then((fetchResponse) => {
-            if(fetchResponse.ok) return fetchResponse
-            else if (!fetchResponse.ok) return fetchResponse
-            else throw new Error("Status code error: " + res.status)
-        })
-        .catch(err=>console.log('error log',err));
-        
-        console.log('fetchResponse',fetchResponse)
-        if (fetchResponse !== undefined) {
-           json  = await fetchResponse.json();
-        } else {json = 'Error'
-        fetchResponse = {}
-        fetchResponse.status = false  
-        }      
-        console.log('id',api.api_id,'json', json,'get', get_success,'fetch status', fetchResponse.status)
-            // console.log(data);
-        updateApiResponse( api.api_id, json, get_success, fetchResponse.status);
-        
-        // if(json != undefined) { get_success = true; } // not very good
-        
+        getApi(api);
     }
+
 
     const updatedResponse = await query(`SELECT * FROM API_LIST INNER JOIN API_RESPONSE ON api_list.api_id=api_response.api_id`);
     return updatedResponse.rows;
+}
+
+async function getApi(api) {
+    console.log("getApi()");
+    let json;
+    let status;
+    let get_success = false;
+    
+    let fetchResponse = await fetch(api.api_url);
+
+    /*
+    .then((fetchResponse) => {
+        if(fetchResponse.ok) return fetchResponse
+        else if (!fetchResponse.ok) return fetchResponse
+        else throw new Error("Status code error: " + res.status)
+    })
+    .catch(err=>console.log('error log',err)); */
+    
+    //console.log('fetchResponse',fetchResponse)
+    if (fetchResponse !== undefined) {
+        try {
+            JSON.parse(fetchResponse);
+        } catch (e) {
+            updateApiResponse(api.api_id, false, false, 404, false);
+            return;
+        }
+        updateApiResponse(api.api_id, "", true, 200, true);
+    }
 }
 
 /* Create a new API entry */
@@ -50,10 +55,10 @@ export async function createApi(api) {
 }
 
 /* Update the API table */
-export async function updateApiResponse(id, json, get_success, response_code) {
+export async function updateApiResponse(id, json, get_success, response_code, response_status) {
     //console.log(`response ${response_code}`);
     //console.log(`id ${id}`);
-    const response = await query(`UPDATE API_RESPONSE SET json=$1, get=$2, response_code=$3 WHERE api_id=$4 RETURNING *`, [id, json, get_success, response_code]);
+    const response = await query(`UPDATE API_RESPONSE SET json=$1, get=$2, response_code=$3, response=$4 WHERE api_id=$5 RETURNING *`, [json, get_success, response_code, response_status, id]);
     return response.rows;
 }
 
